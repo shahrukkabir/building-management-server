2fce8da77607a0c6d2da75986b30597ecd781e16
Shahruk Kabir <shahrukkabir01@gmail.com>
2025-05-12 10:33:15 +0600

    setup MongoDB connection

--- index.js
+++ index.js
@@ -10,6 +10,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB!");
+
+    const database = client.db("yourDatabaseName");
+    const collection = database.collection("yourCollectionName");
+    // You can add your API routes here
+
+  } 
+  catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+}
+
+//run
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Server is up and running!");
 });
